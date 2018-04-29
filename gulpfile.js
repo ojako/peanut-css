7a74c1921c67441a655fb78e84d6c73989d94942
JakO <jk.sbrn@gmail.com>
2018-04-29 16:51:16 +0100

    better tooling and move all html to pug

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,42 @@
 
 'use strict';
 
-let gulp = require('gulp');
-let cleanCSS = require('gulp-clean-css');
-let gulpPug = require('gulp-pug');
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const cleanCSS = require('gulp-clean-css');
+const scss = require('gulp-sass');
+const pug = require('gulp-pug');
+const del = require('del');
 
-gulp.task('default', defaultTask);
+// Files to be processed
+const paths = {
+  generatedFiles: {
+    src: [
+      'dist', 
+      'index.html',
+    ],
+  },
+  styles: {
+    src: 'src/peanut.css',
+    dest: 'dist/peanut.css',
+  },
+  templates: {
+    src: 'templates/index.pug',
+    dest: '',
+  },
+}
+
+// Kill generated files
+function clean() {
+  return del(paths.generatedFiles.src);
+}
+
+function watch() {
+  gulp.watch([
+    paths.styles.src,
+    paths.templates.src,
+  ])
+}
 
 gulp.task('minify', () => {
   return gulp.src('src/peanut.css')
@@ -19,15 +50,32 @@ gulp.task('minify', () => {
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('watch', () => {
+  return watch('src/**/*', [
+    'views',
+    'cssSASS',
+  ]);
+});
+
+gulp.task('cssSASS', () => {
+   return gulp.src('src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
 gulp.task('views', () => {
-  return gulp.src('templates/*.pug')
-    .pipe(gulpPug({
+  return gulp.src('templates/index.pug')
+    .pipe(pug({
       pretty: true,
     }))
-    .pipe(gulp.dest('templates'));
-    ;
-    
-})
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', defaultTask);
+
+//watch() => {
+//  //
+//}
 
 function defaultTask(done) {
   done();
